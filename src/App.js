3800123b276976d288cb38049e78df2e8d156b86
Esteban Landucci <emlanducci@gmail.com>
2022-09-16 18:20:09 -0300

    configurando entorno con tailwind

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="text-3xl font-mono font-bold underline">
+          Hello bitches xD
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn your place
         </a>
       </header>
     </div>
