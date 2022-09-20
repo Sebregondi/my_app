5e0584d15ecf486f5138fab1a94690c7c71c33f8
Esteban Landucci <emlanducci@gmail.com>
2022-09-19 23:07:27 -0300

    primera pre entrega casi lista

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Saludo from './Saludo';
 import ChildrenBox from './components/ChildrenBox';
+import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
 
@@ -12,22 +14,9 @@ function App() {
 
   return (
     <>
-      <h1>¡Hello bitches! xD</h1>
-      <ChildrenBox>
-        <p>Parrafo con texto</p>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ChildrenBox>
-      <h2 style={styles}>Subtítulo</h2>
-      <li className='green'>elemento 1</li>
-      <li>elemento 2</li>
-      <li>elemento 3</li>
-      <span>otra cosa</span>
-      <strong>en negrita</strong>
-      <Saludo name='Miguel' age={23}/>
-      <Saludo name='Jorge' age={18}></Saludo>
-      <Saludo name='Robinson' age={28}></Saludo>
+    <NavBar />
+    <ItemListContainer 
+      greeting={"Mi nombre es Esteban Landucci, de la comisión 38095"}/>
     </>
   );
 }
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItemListContainer = ( {greeting} ) => {
+  return (
+    <span className='font-mono m-5'> {greeting}</span>
+  )
+}
+
+export default ItemListContainer
