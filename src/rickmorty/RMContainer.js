bb46b9aa59a17faca824bc568571a7ccf755f085
Esteban Landucci <emlanducci@gmail.com>
2022-10-02 16:57:09 -0300

    only rick cards

--- src/rickmorty/RMContainer.js
+++ src/rickmorty/RMContainer.js
@@ -20,7 +20,7 @@ const RMContainer = () => {
 
 // FETCH RESOLVIENDO DOS PROMESAS
  const getChars = () => {
-   const URL = 'https://rickandmortyapi.com/api/character?page=4'
+   const URL = 'https://rickandmortyapi.com/api/character?name=rick'
    fetch (URL)
     .then ( res => res.json () )
     .then ( data => {
@@ -34,19 +34,16 @@ return (
     <div>
         { chars.map( c => 
       <div key={c.id} className="card card-normal lg:card-side bg-base-100 shadow-x5 bordered">
-      <figure><img src={c.image} alt={c.name}/></figure>
-      <div className="card-body">
-        <h1 className="card-title">{c.name}</h1>
-        <p><b>Status:</b> {c.status}</p>
-        <p><b>Species:</b> {c.species}</p>
-        <p><b>Gender:</b> {c.gender}</p>
-        <p><b>Location:</b> {c.location.name}</p>
-        
-        <div className="card-actions justify-end">
-          <button className="btn bg-error">Listen</button>
+        <figure><img src={c.image} alt={c.name}/></figure>
+        <div className="card-body">
+          <h1 className="card-title bg-error pl-5">{c.name}</h1>
+          <p><b>Status:</b> {c.status}</p>
+          <p><b>Species:</b> {c.species}</p>
+          <p><b>Gender:</b> {c.gender}</p>
+          <p><b>Location:</b> {c.location.name}</p>
+
         </div>
-      </div>
-    </div>)}
+      </div>)}
     </div>
   )
 }
