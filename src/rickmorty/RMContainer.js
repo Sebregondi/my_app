41f5ba4375f5c350a645bfe4a73cdf689bf2c9e6
Esteban Landucci <emlanducci@gmail.com>
2022-10-03 20:50:19 -0300

    arreglos menores

--- src/rickmorty/RMContainer.js
+++ src/rickmorty/RMContainer.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
-export const RMCard = ({name, image}) => {
+export const RMCard = ({ name, image }) => {
   return (
     <div>
-        <div>{name}</div>
-        <img src={image} />
+      <div>{name}</div>
+      <img src={image} />
     </div>
   )
 }
@@ -16,33 +16,32 @@ const RMContainer = () => {
 
   useEffect(() => {
     getChars()
-}, [])
+  }, [])
 
-// FETCH RESOLVIENDO DOS PROMESAS
- const getChars = () => {
-   const URL = 'https://rickandmortyapi.com/api/character?name=rick'
-   fetch (URL)
-    .then ( res => res.json () )
-    .then ( data => {
-        console.log( data.results );
-        setChars( data.results )
-    })
-    .catch ( err => {console.log(err);})
-}
+  // FETCH RESOLVIENDO DOS PROMESAS
+  const getChars = () => {
+    const URL = 'https://rickandmortyapi.com/api/character?name=rick'
+    fetch(URL)
+      .then(res => res.json())
+      .then(data => {
+        setChars(data.results)
+      })
+      .catch(err => { console.log(err); })
+  }
 
-return (
+  return (
     <div>
-        { chars.map( c => 
-      <div key={c.id} className="font-mono card card-normal lg:card-side bg-base-100 shadow-x5 mt-2">
-        <figure><img className='mask mask-decagon' src={c.image} alt={c.name}/></figure>
-        <div className="card-body">
-          <h1 className="card-title bg-error pl-5">{c.name}</h1>
-          <p><b>Status:</b> {c.status}</p>
-          <p><b>Species:</b> {c.species}</p>
-          <p><b>Gender:</b> {c.gender}</p>
-          <p><b>Location:</b> {c.location.name}</p>
-        </div>
-      </div>)}
+      {chars.map(c =>
+        <div key={c.id} className="font-mono card card-normal lg:card-side bg-base-100 shadow-x5 mt-2">
+          <figure><img className='mask mask-decagon' src={c.image} alt={c.name} /></figure>
+          <div className="card-body">
+            <h1 className="card-title bg-error pl-5">{c.name}</h1>
+            <p><b>Status:</b> {c.status}</p>
+            <p><b>Species:</b> {c.species}</p>
+            <p><b>Gender:</b> {c.gender}</p>
+            <p><b>Location:</b> {c.location.name}</p>
+          </div>
+        </div>)}
     </div>
   )
 }
@@ -58,19 +57,19 @@ export default RMContainer
 // }
 
 // FETCH CON AXIOS Y ASYNC Y AWAIT
-// const getChars = async () => { 
+// const getChars = async () => {
 //     const URL = 'https://rickandmortyapi.com/api/character'
-//     const response = await axios.get( URL ) 
+//     const response = await axios.get( URL )
 //     setChars ( response.data.results)
 // }
 
 // FETCH CON AXIOS
-// const getChars = () => { 
+// const getChars = () => {
 //   const URL = 'https://rickandmortyapi.com/api/character'
 //   axios.get( URL ).then( res =>{
 //     console.log(res.data.results);
 //     setChars (res.data.results)
-//   }) 
+//   })
 // }
 
 // FETCH CON ASYNC Y AWAIT
